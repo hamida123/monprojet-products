9e3ee6daeb708950c6af200559f7b085e67b172c
Hamida TLILI <tlilihamida1@gmail.com>
2018-04-05 09:45:03 +0200

    service rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Route, RoutesRecognized} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,6 +8,19 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductService } from './services/product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFountComponent } from './components/not-fount/not-fount.component';
+
+
+
+
+const routes: Route[] =[
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'list', component: ProductListComponent},
+  {path: '', component: WelcomeComponent, pathMatch: 'full'},
+  {path: '**', component: NotFountComponent}
+];//w'll be
+
 
 
 @NgModule({
@@ -16,10 +29,13 @@ import { HttpClientModule } from '@angular/common/http';
     NavbarComponent,
     ProductListComponent,
     FooterComponent,
+    NotFountComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
